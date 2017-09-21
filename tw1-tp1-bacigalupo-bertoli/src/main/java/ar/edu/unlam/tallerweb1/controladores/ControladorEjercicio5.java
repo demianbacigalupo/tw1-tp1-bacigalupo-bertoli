2b28b128348abded8d21e9db5a0434791d865ac4
berto2904 <berto2904@gmail.com>
2017-09-20 21:53:28 -0300

    Arreglo del ejercicio 5

--- tw1-tp1-bacigalupo-bertoli/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorEjercicio5.java
+++ tw1-tp1-bacigalupo-bertoli/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorEjercicio5.java
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorEjercicio5 {
-	@RequestMapping("/ejercicio5")
-	public ModelAndView Saludar(@PathVariable("numero1") String num1,@RequestParam("numero2") String num2){
-		String num3;
+	@RequestMapping("/ejercicio5/{numero1}/{numero2}")
+	public ModelAndView Saludar(@PathVariable("numero1") Long num1 ,@PathVariable("numero2") Long num2){
+		Long num3;
 		ModelMap modelo = new ModelMap();
 		modelo.addAttribute("numsum1", num1);
 		modelo.addAttribute("numsum2", num2);
